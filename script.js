709ac9b2430fd8b03d0712895d98fa26660cd3e1
= <maria.louisa.castro@gmail.com>
2022-01-25 23:15:10 +0000

    Add JS to make instructions appear onclick of help button
    Realtes #1

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function exitInstructions(e) {
   const instructions = document.getElementById("instructions");
+  instructions.classList.remove("visible");
   instructions.classList.add("hidden");
+}
+
+function showInstrustions(e) {
+  const instructions = document.getElementById("instructions");
+  instructions.classList.remove("hidden");
+  instructions.classList.add("visible");
 }
